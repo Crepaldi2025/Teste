b04e5dba786bbbd41b593a8ea1cbd51cc05e83a5
Paulo Cesar Crepaldi <crepaldipaulocesar@gmail.com>
2026-05-06 14:37:25 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import json
+import streamlit as st
+import ee
+from google.oauth2 import service_account
+
+st.set_page_config(page_title="Teste GEE", layout="centered")
+
+@st.cache_resource
+def init_gee():
+    info = json.loads(st.secrets["gee_service_account_json"])
+
+    credentials = service_account.Credentials.from_service_account_info(
+        info,
+        scopes=[
+            "https://www.googleapis.com/auth/earthengine",
+            "https://www.googleapis.com/auth/cloud-platform",
+        ],
+    )
+
+    ee.Initialize(credentials, project=st.secrets["gee_project_id"])
+    return ee
+
+st.title("Teste de conexão com Google Earth Engine")
+
+try:
+    ee = init_gee()
+    st.success("Google Earth Engine inicializado com sucesso.")
+
+    ponto = ee.Geometry.Point([-45.45, -22.42])
+
+    imagem = ee.Image("CGIAR/SRTM90_V4")
+    resultado = imagem.reduceRegion(
+        reducer=ee.Reducer.mean(),
+        geometry=ponto.buffer(1000),
+        scale=90,
+        maxPixels=1e9
+    ).getInfo()
+
+    st.write("Resultado do teste:")
+    st.json(resultado)
+
+except Exception as e:
+    st.error(f"Erro ao inicializar o GEE: {e}")
